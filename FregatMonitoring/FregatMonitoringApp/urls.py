ce8e56843767477af0a98e2f14dea155657b9386
AlexRubilnik <rubilnik89@yandex.ru>
2021-11-12 15:40:26 +0300

    Создал шаблон для отображения данных о печи и view для этого шаблона для каждой из печей

--- FregatMonitoring/FregatMonitoringApp/urls.py
+++ FregatMonitoring/FregatMonitoringApp/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('Furnace_1_power/', views.Furnace_1_power, name='Furnace_1_power'),
-]    
+    path('Furnace_1_info/', views.Furnace_1_info, name='Furnace_1_info'),
+    path('Furnace_2_info/', views.Furnace_2_info, name='Furnace_2_info'),
+]   
--- FregatMonitoring/FregatMonitoringApp/views.py
+++ FregatMonitoring/FregatMonitoringApp/views.py
@@ -1,11 +1,54 @@
+from django import template
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Floattable, Tagtable
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello! This is FregatMonitoring system")
 
-def Furnace_1_power(request):
-    tag = Tagtable.objects.filter(tagname='MEASURES\HY_F711')
-    last_value = Floattable.objects.filter(tagindex=tag[0].tagindex).order_by('-dateandtime')[:1]
-    return HttpResponse('Задание мощности печь №1: %s ' % last_value[0].val)
+def Furnace_1_info(request):
+    
+    tag_list = list()
+    tag_list.append(['Задание мощности', Tagtable.objects.filter(tagname='MEASURES\HY_F711')[0].tagindex])
+    tag_list.append(['Расход газа', Tagtable.objects.filter(tagname='MEASURES\FL710_NG')[0].tagindex])
+    tag_list.append(['Расход воздуха', Tagtable.objects.filter(tagname='MEASURES\FL710_AIR')[0].tagindex])
+    tag_list.append(['Расход кислорода', Tagtable.objects.filter(tagname='MEASURES\O1Flow')[0].tagindex])
+    tag_list.append(['Альфа', Tagtable.objects.filter(tagname='MEASURES\Alpha_p1')[0].tagindex])
+    tag_list.append(['Лямбда', Tagtable.objects.filter(tagname='MEASURES\Lambda_p1')[0].tagindex])
+    tag_list.append(['Перепад на фильтре', Tagtable.objects.filter(tagname='MEASURES\PDI_720')[0].tagindex])
+    tag_list.append(['Перепад на дымососе', Tagtable.objects.filter(tagname='MEASURES\PDI_724')[0].tagindex])
+    tag_list.append(['Разяряжение в гор. газоходе', Tagtable.objects.filter(tagname='MEASURES\PI_701')[0].tagindex])
+    tag_list.append(['Частота ПЧ дымососа', Tagtable.objects.filter(tagname='MEASURES\SI_U720')[0].tagindex])
+
+    for tag in tag_list:
+        tag.append(Floattable.objects.filter(tagindex=tag[1]).order_by('-dateandtime')[0].val)
+
+    template = loader.get_template('FregatMonitoringApp/Furnace_info.html')
+    tag_dict = {'tags_values': tag_list}
+    context = tag_dict
+
+    return HttpResponse(template.render(context, request))
+
+def Furnace_2_info(request):
+    
+    tag_list = list()
+    tag_list.append(['Задание мощности', Tagtable.objects.filter(tagname='MEASURES\HY_F710')[0].tagindex])
+    tag_list.append(['Расход газа', Tagtable.objects.filter(tagname='MEASURES\TI_810B')[0].tagindex])
+    tag_list.append(['Расход воздуха', Tagtable.objects.filter(tagname='MEASURES\TI_810C')[0].tagindex])
+    tag_list.append(['Расход кислорода', Tagtable.objects.filter(tagname='MEASURES\O2Flow')[0].tagindex])
+    tag_list.append(['Альфа', Tagtable.objects.filter(tagname='MEASURES\Alpha')[0].tagindex])
+    tag_list.append(['Лямбда', Tagtable.objects.filter(tagname='MEASURES\Lambda')[0].tagindex])
+    tag_list.append(['Перепад на фильтре', Tagtable.objects.filter(tagname='MEASURES\PDI_725')[0].tagindex])
+    tag_list.append(['Перепад на дымососе', Tagtable.objects.filter(tagname='MEASURES\PDI_729')[0].tagindex])
+    tag_list.append(['Разяряжение в гор. газоходе', Tagtable.objects.filter(tagname='MEASURES\PI_702')[0].tagindex])
+    tag_list.append(['Частота ПЧ дымососа', Tagtable.objects.filter(tagname='MEASURES\SI_U721')[0].tagindex])
+
+    for tag in tag_list:
+        tag.append(Floattable.objects.filter(tagindex=tag[1]).order_by('-dateandtime')[0].val)
+
+    template = loader.get_template('FregatMonitoringApp/Furnace_info.html')
+    tag_dict = {'tags_values': tag_list}
+    context = tag_dict
+
+    return HttpResponse(template.render(context, request))
